167f0578b61cadbd383cb88b86372969f7b1108a
RivTian <kannajiahe@gmail.com>
2024-01-29 20:51:05 +0800

    feat. 新增过滤条件，隐藏文件不显示

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with('.'))
+        .unwrap_or(false)
+}
 
 fn main() {
-    for entry in WalkDir::new(".") {
+    // TODO Do This or That:
+    for entry in WalkDir::new(".")
+        .min_depth(1)
+        .max_depth(1)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+    {
         let entry = entry.unwrap();
         println!("{}", entry.path().display());
     }
 }
-
